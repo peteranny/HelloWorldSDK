70401bdab793d42ebdbde458c4863d4ab6a57f6d
peteranny <peteranny2@gmail.com>
2019-07-23 16:43:04 +0800

    create framework project

---
+++ HelloWorldSDK/HelloWorldSDK.h
@@ -0,0 +1,19 @@
+//
+//  HelloWorldSDK.h
+//  HelloWorldSDK
+//
+//  Created by peter.shih on 2019/8/26.
+//  Copyright © 2019年 Peteranny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelloWorldSDK.
+FOUNDATION_EXPORT double HelloWorldSDKVersionNumber;
+
+//! Project version string for HelloWorldSDK.
+FOUNDATION_EXPORT const unsigned char HelloWorldSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorldSDK/PublicHeader.h>
+
+
